5e262945eb2f3862968576180d50fc3249dfade0
BRUNO ALVES SIQUEIRA <brzero2016@gmail.com>
2025-12-12 15:49:39 -0300

    criar web componente toolbar

---
+++ sistema-integracao/src/app/toolbar-integracao/toolbar-integracao.component.ts
@@ -0,0 +1,13 @@
+import { Component, ChangeDetectionStrategy  } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar-integracao',
+  standalone: true,
+  imports: [],
+  templateUrl: './toolbar-integracao.component.html',
+  styleUrl: './toolbar-integracao.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ToolbarIntegracaoComponent {
+
+}
---
+++ sistema-integracao/src/main.ts
@@ -0,0 +1,16 @@
+import { createApplication } from '@angular/platform-browser';
+import { createCustomElement } from '@angular/elements';
+import { ToolbarIntegracaoComponent } from './app/toolbar-integracao/toolbar-integracao.component';
+import { provideZoneChangeDetection, provideZonelessChangeDetection } from '@angular/core';
+
+(async () => {
+  const app = await createApplication({ providers: [
+    provideZonelessChangeDetection(),
+  ] });
+
+  const ToolbarEl = createCustomElement(ToolbarIntegracaoComponent, {
+    injector: app.injector,
+  });
+
+  customElements.define('toolbar-integracao', ToolbarEl);
+})();
